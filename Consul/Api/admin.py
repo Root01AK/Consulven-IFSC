78a0ea871ea687771748410d7394d00d51d9ef23
Kiruba karan A <kirubakaran@trdfin.in>
2025-01-15 16:52:23 +0530

    third commit

--- Consul/Api/admin.py
+++ Consul/Api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import career, Enquiry, contact
 # Register your models here.
+
+
+admin.site.register(contact)
+admin.site.register(Enquiry)
+admin.site.register(career)
